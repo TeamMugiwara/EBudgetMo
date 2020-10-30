ee7529c398ddcf8183cc40a1b46e3d856fe00e94
teammugiwara <teammugiwara28@gmail.com>
2020-10-30 12:13:11 +0600

    Splash Screen and Home UI Design

---
+++ EBudgetMo/app/src/main/java/edu/ucucite/ebudgetmo/MainActivity.java
@@ -0,0 +1,33 @@
+package edu.ucucite.ebudgetmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+    private static int SPLASH_TIME_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent=new Intent(MainActivity.this,home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+
+    }
+}
