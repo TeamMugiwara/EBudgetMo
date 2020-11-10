5873460540ec249de424f112a85d7fc4eefce6e9
teammugiwara <teammugiwara28@gmail.com>
2020-11-10 18:52:04 +0600

    Created all needed activities UI design for some activities Codes for buttons

---
+++ EBudgetMo/app/src/main/java/edu/ucucite/ebudgetmo/Mylist.java
@@ -0,0 +1,14 @@
+package edu.ucucite.ebudgetmo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mylist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mylist);
+    }
+}
--- EBudgetMo/app/src/main/java/edu/ucucite/ebudgetmo/home.java
+++ EBudgetMo/app/src/main/java/edu/ucucite/ebudgetmo/home.java
@@ -2,7 +2,9 @@ package edu.ucucite.ebudgetmo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class home extends AppCompatActivity {
 
@@ -10,5 +12,30 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void btnexpense(View view) {
+        Intent intent = new Intent(this, Expense.class);
+        startActivity(intent);
+    }
+
+    public void btnlist(View view) {
+        Intent intent = new Intent(this, Mylist.class);
+        startActivity(intent);
+    }
+
+    public void btnbills(View view) {
+        Intent intent = new Intent(this, Bills.class);
+        startActivity(intent);
+    }
+
+    public void btnincome(View view) {
+        Intent intent = new Intent(this, Income.class);
+        startActivity(intent);
+    }
+
+    public void btnsavings(View view) {
+        Intent intent = new Intent(this, Savings.class);
+        startActivity(intent);
     }
 }
